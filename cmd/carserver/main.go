69541bab695567294fb9b141cfbb1f320c33a119
atharvOzone <atharva@ozone.one>
2023-06-12 12:02:36 +0530

    Improved Project Structure

---
+++ cmd/carserver/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	// "crud-go/controllers"
+	"crud-go/pkg/initializers"
+	"crud-go/pkg/middleware"
+	"crud-go/pkg/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	r := gin.Default()
+	//CAR APIS
+    r.POST("/cars", middleware.RequireAuth ,controllers.CarsCreate)
+	r.GET("/cars", middleware.RequireAuth,controllers.GetCars)
+	r.GET("/cars/:id", middleware.RequireAuth,controllers.GetCarByID)
+	r.PUT("/cars/:id", middleware.RequireAuth, controllers.UpdateCarByID)
+	r.DELETE("/cars/:id", middleware.RequireAuth, controllers.CarDelete)
+
+	//USER APIS
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+
+    r.Run() 
+}
+ 
